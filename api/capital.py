2d7a71cb96aa567224239dfae640547c63250528
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-04-13 23:07:09 +0300

    d

--- api/capital.py
+++ api/capital.py
@@ -11,13 +11,13 @@ class handler(BaseHTTPRequestHandler):
         url_components=parse.urlsplit(mypath)
         query_string_list=parse.parse_qsl(url_components.query)
         dic=dict(query_string_list)
-        name=dic["name"].upper()
+        name=dic.get("name").upper()
         my_name=f"{name},{name}!"
 
         self.send_response(200)
         self.send_header('Content-typer','text/plain')
         self.end_headers()
         
-        self.wfile.write((my_name).encode())
+        self.wfile.write(my_name.encode())
          
       
