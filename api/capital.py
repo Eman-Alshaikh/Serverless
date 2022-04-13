fe28837cb0c5231200ba193d95f4f5b000cd6e55
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-04-13 23:11:40 +0300

    d

--- api/capital.py
+++ api/capital.py
@@ -11,13 +11,17 @@ class handler(BaseHTTPRequestHandler):
         url_components=parse.urlsplit(mypath)
         query_string_list=parse.parse_qsl(url_components.query)
         dic=dict(query_string_list)
-        name=dic.get("name").upper()
-        my_name=f"{name},{name}!"
+        name=dic.get("name") 
+       
+        if name :
+            msg=f"i hope for you a nice day {name.upper()}"
+        else:
+            msg="i hope for you a nice day"
 
         self.send_response(200)
         self.send_header('Content-typer','text/plain')
         self.end_headers()
         
-        self.wfile.write(my_name.encode())
+        self.wfile.write(msg.encode())
          
-      
+   
