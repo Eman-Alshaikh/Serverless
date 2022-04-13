3c707ecf8c23a68122a11f42d31ae5024d63e32e
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-04-13 20:33:51 +0300

    c

---
+++ api/capital.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse 
+
+
+class handler(BaseHTTPRequestHandler):
+    
+
+    def do_GET(self):
+        path=self.path 
+        url_components=parse.urlsplit(path)
+        query_string=parse.parse_qsl(url_components.query)
+        dic=dict(query_string)
+        name=dic["name"].upper()
+        my_name=f"{name},{name}!"
+
+        self.send_response(200)
+        self.send_header('Content-typer','text/plain')
+        self.end_headers()
+        
+        self.wfile.write((my_name).encode())
+        return
